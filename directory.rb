5c3cc6cf62d104ea03b3f83d55c2c75f9d0989fd
ana carolina <anacarolinarodriguesoares@gmail.com>
2023-03-07 15:03:33 +0000

    Creating methods

--- directory.rb
+++ directory.rb
@@ -3,12 +3,23 @@ students = [
   "Alex DeLarge","The Wicked Witch of the West","Terminator",
   "Freddy Krueger","The Joker","Joffrey Baratheon","Norman Bates"
 ]
-puts "The students of Villains Academy"
-puts "-----------"
 
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-----------"
 end
 
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
 #printing the total number of students
-print "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
