85e56cbe3b55ea96e800d9e3588fe26bb8ed9123
ana carolina <anacarolinarodriguesoares@gmail.com>
2023-03-09 15:42:31 +0000

    Loading the data from the file

--- directory.rb
+++ directory.rb
@@ -27,7 +27,7 @@ def input_students
 end
 
 def create_student_hash(name,age,hobbie,cohort = 'undefined cohort')
-    {name: name, cohort: cohort.to_s, age: age, hobbie: hobbie}
+    {name: name, cohort: cohort.to_sym, age: age, hobbie: hobbie}
 end
 
 def print_header
@@ -66,6 +66,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list of students.csv"
+  puts "4. Load the list of students.csv"
   puts "9. Exit"
 end
 
@@ -92,6 +93,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def process(selection)
   case selection
   when "1"
@@ -100,6 +110,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
